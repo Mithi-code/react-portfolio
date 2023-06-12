16aa9b68249fccba79de62fdbf1a71474f5cb0da
Mithi-code <mithlesh.9m@gmail.com>
2023-06-12 15:52:52 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<h1 style={{color: 'red'}}>Go back to home</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
