4c4bd6405e52c39b5c0f165bdcf2d11753cffe8f
Mithi-code <mithlesh.9m@gmail.com>
2023-07-23 10:46:08 +0530

    react app initialize

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
-
-import StarsCanvas from "./components";
-
-import Contact from "./components/Contact";
-import Feedbacks from "./components/Feedbacks";
-import Hero from "./components/Hero";
-import Works from "./components/Work";
-import Tech from "./components/Tech";
-
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Navbar from "./components/Navbar";
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+     Hello Mithi
+    </div>
   );
-};
+}
 
 export default App;
